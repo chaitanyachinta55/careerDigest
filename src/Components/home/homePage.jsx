0f9a9fc36bf89b4a2f7913bd7f214c3adfefe1ed
chaitanyachinta <chaitanyachinta55@gmail.com>
2020-06-28 20:06:10 +0530

    timeline changes

--- src/Components/home/homePage.jsx
+++ src/Components/home/homePage.jsx
@@ -14,15 +14,26 @@ class homePage extends Component{
             if(!this.state.hideDetailedScroll && window.scrollY >= 40){
                 document.getElementById("scroll").classList.toggle("fadeOut");
                 document.getElementById("identityInfo").classList.toggle("fadeOut");
+                document.getElementById("overview").classList.toggle("fadeOut");
                 document.getElementById("header").classList.toggle("fadeIn");
+                document.getElementById("timeline").classList.toggle("timelineActive");
+                
+                //BELOW CODE SHOULD HELP IN ANIMATING ONE AFTER THE OTHER
+                const eachline = document.querySelectorAll('.timeline li');
+                console.log(eachline);
+                eachline.forEach((li , index) => {
+                    li.style.animation = `timelineFadein 2s ease forwards ${index / 9}s`
+                });
                 this.setState({hideDetailedScroll : true});
             }
 
-            if(this.state.hideDetailedScroll && window.scrollY == 0){
+            if(this.state.hideDetailedScroll && window.scrollY === 0){
                 console.log(window.scrollY);
                 document.getElementById("scroll").classList.toggle("fadeOut");
                 document.getElementById("identityInfo").classList.toggle("fadeOut");
+                document.getElementById("overview").classList.toggle("fadeOut");
                 document.getElementById("header").classList.toggle("fadeIn");
+                document.getElementById("timeline").classList.toggle("timelineActive");
                 this.setState({hideDetailedScroll : false});
             }
         })
@@ -34,7 +45,7 @@ class homePage extends Component{
                 <section>
                     < div id = "headerInfo" className="headerInfo">
                         <div className="headerDP">
-                            <img className="headerImage" src = "/images/profilepic.jpg"/>
+                            <img className="headerImage" src = "/images/profilepic.jpg" alt=""/>
                         </div>
                         <h1 className="headerName">Chaitanya Chinta</h1>
                     </div>
@@ -44,25 +55,34 @@ class homePage extends Component{
                 <section>
                     <div id = "identityInfo" className="identityInfo">
                         <div className="profileDP">
-                            <img className="dPImage" src = "/images/profilepic.jpg"/>
+                            <img className="dPImage" src = "/images/profilepic.jpg" alt=""/>
                         </div>
                         <h1 className="fullname">Chaitanya Chinta</h1>
                     </div>
                 </section>
-                        <h4 id = "scroll" className="scroll">Scroll for Detailed Info</h4>
+                
+                <h4 id = "scroll" className="scroll">Scroll for Detailed Info</h4>
 
-                    <div className="overview">
-                        <ul>
-                            <li>6.5 Years Experience</li>
-                            <li>Current Organization : Hitachi Consulting</li>
-                            <li>Current Designation : Sr Software Engineer</li>
-                            <li>Skill Set : <p> JAVA , SPRING , HIBERNATE , SQL , STORED PROCEEDURES , <br/> 
-                                REACTJS , NODEJS , EXPRESS , ANGULARJS , SMART COMMUNICATIONS</p></li>
-                        </ul>
-                    </div>
+                <div id = "overview" className="overview">
+                    <ul>
+                        <li>6.5 Years Experience</li>
+                        <li>Current Organization : Hitachi Consulting</li>
+                        <li>Current Designation : Sr Software Engineer</li>
+                        <li>Skill Set : <p> JAVA , SPRING , HIBERNATE , SQL , STORED PROCEEDURES , <br/> 
+                            REACTJS , NODEJS , EXPRESS , ANGULARJS , SMART COMMUNICATIONS</p></li>
+                    </ul>
+                </div>
             </div>
             <div className="detailedInfo">
-
+                <div id = "timeline" className="timeline">
+                    <ul>
+                        <li>2019</li>
+                        <li>2018</li>
+                        <li>2017</li>
+                        <li>2014</li>
+                    </ul>
+                </div>
+                <div className="detailsPanel"></div>
             </div>
             
             <div className="footer">
